f9207b71862fa8ecb4c2165caeeab6cfe9616909
kang-min-seok <min1042004444@gmail.com>
2023-04-08 14:14:17 +0900

    강좌 2강

--- rhythmOrchestra/src/rhythmOrchestra/RhythmOrchestra.java
+++ rhythmOrchestra/src/rhythmOrchestra/RhythmOrchestra.java
@@ -1,8 +1,18 @@
 package rhythmOrchestra;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class RhythmOrchestra extends JFrame{
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
+	
 	public RhythmOrchestra() {
 		setTitle("RhythmOrchestra");
 		setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
@@ -10,5 +20,19 @@ public class RhythmOrchestra extends JFrame{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(title).jpg")).getImage();
+	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
 	}
 }
