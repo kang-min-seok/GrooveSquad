6bcde2bb35fa5d9008eda2e26c2940d49585b859
kang-min-seok <min1042004444@gmail.com>
2023-04-11 18:15:30 +0900

    강좌 6강

--- rhythmOrchestra/src/rhythmOrchestra/RhythmOrchestra.java
+++ rhythmOrchestra/src/rhythmOrchestra/RhythmOrchestra.java
@@ -25,6 +25,13 @@ public class RhythmOrchestra extends JFrame{
 	private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonEntered.png"));
 	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/quitButtonBasic.png"));
 	
+	private ImageIcon leftButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/leftButtonEntered.png"));
+	private ImageIcon leftButtonBasicImage = new ImageIcon(Main.class.getResource("../images/leftButtonBasic.png"));
+	private ImageIcon rightButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/rightButtonEntered.png"));
+	private ImageIcon rightButtonBasicImage = new ImageIcon(Main.class.getResource("../images/rightButtonBasic.png"));
+	
+	private Image titleImage = new ImageIcon(Main.class.getResource("../images/wondersTitleImage.png")).getImage();
+	private Image selectedImage= new ImageIcon(Main.class.getResource("../images/wondersSelectImage.jpg")).getImage();
 	
 	private Image background= new ImageIcon(Main.class.getResource("../images/introBackground(title).jpg")).getImage();
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
@@ -33,8 +40,11 @@ public class RhythmOrchestra extends JFrame{
 	private JButton startButton = new JButton (startButtonBasicImage);
 	private JButton quitButton = new JButton (quitButtonBasicImage);
 	
-	
+	private JButton leftButton = new JButton (leftButtonBasicImage);
+	private JButton rightButton = new JButton (rightButtonBasicImage);
 	private int mouseX, mouseY;
+	
+	private boolean isMainScreen = false;
 	
 	public RhythmOrchestra() {
 		setUndecorated(true);
@@ -102,8 +112,10 @@ public class RhythmOrchestra extends JFrame{
 				// 게임 시작 이벤트
 				startButton.setVisible(false);
 				quitButton.setVisible(false);
+				leftButton.setVisible(true);
+				rightButton.setVisible(true);
 				background = new ImageIcon(Main.class.getResource("../images/mainBackground.jpg")).getImage();
-				
+				isMainScreen = true;
 			}
 		});
 		add(startButton);
@@ -141,7 +153,59 @@ public class RhythmOrchestra extends JFrame{
 		add(quitButton);
 		
 		
+		leftButton.setVisible(false);
+		leftButton.setBounds(140, 310, 60, 60);
+		leftButton.setBorderPainted(false);
+		leftButton.setContentAreaFilled(false);
+		leftButton.setFocusPainted(false);
+		leftButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				leftButton.setIcon(leftButtonEnteredImage);
+				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3",false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				leftButton.setIcon(leftButtonBasicImage);
+				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonPressedMusic = new Music("buttonPressedMusic.mp3",false);
+				buttonPressedMusic.start();
+				//왼쪽 버튼 이벤트
+			}
+		});
+		add(leftButton);
 		
+		rightButton.setVisible(false);
+		rightButton.setBounds(1080, 310, 60, 60);
+		rightButton.setBorderPainted(false);
+		rightButton.setContentAreaFilled(false);
+		rightButton.setFocusPainted(false);
+		rightButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rightButton.setIcon(rightButtonEnteredImage);
+				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3",false);
+				buttonEnteredMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rightButton.setIcon(rightButtonBasicImage);
+				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonPressedMusic = new Music("buttonPressedMusic.mp3",false);
+				buttonPressedMusic.start();
+				//오른쪽 버튼 이벤트
+			}
+		});
+		add(rightButton);
 		
 		menuBar.setBounds(0,0,1280,30);
 		menuBar.addMouseListener(new MouseAdapter() {
@@ -161,17 +225,8 @@ public class RhythmOrchestra extends JFrame{
 		});
 		add(menuBar);
 	
-
-		
-		
-		
-		
 		
 		Music introMusic = new Music("introMusic.mp3",true);
-		/*  The Inspiration by Keys of Moon | https://soundcloud.com/keysofmoon
-			Attribution 4.0 International (CC BY 4.0)
-			https://creativecommons.org/licenses/by/4.0/
-			Music promoted by https://www.chosic.com/free-music/all/  */
 		introMusic.start();
 	}
 	
@@ -184,6 +239,10 @@ public class RhythmOrchestra extends JFrame{
 	
 	public void screenDraw(Graphics g) {
 		g.drawImage(background, 0, 0, null);
+		if(isMainScreen) {
+			g.drawImage(selectedImage, 340, 100, null);
+			g.drawImage(titleImage, 340, 70, null);
+		}
 		paintComponents(g);
 		this.repaint();
 	}
