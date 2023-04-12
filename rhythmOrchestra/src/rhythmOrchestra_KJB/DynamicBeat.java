03648a6aa5aacb69a767228d31b3c71c04257445
김재빈 <@DESKTOP-MPNQ6SQ>
2023-04-12 12:23:38 +0900

    김재빈 강좌 1~2강

---
+++ rhythmOrchestra/src/rhythmOrchestra_KJB/DynamicBeat.java
@@ -0,0 +1,15 @@
+package rhythmOrchestra_KJB;
+
+import javax.swing.JFrame;
+
+public class DynamicBeat extends JFrame{
+	public DynamicBeat() {
+		setTitle("Dynamic Beat");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+	}
+}
