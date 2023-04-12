7f788090d9abf987601a9f5d35e3117ecc5685dc
김재빈 <@DESKTOP-MPNQ6SQ>
2023-04-12 12:43:18 +0900

    김재빈 강좌 1~2강

--- rhythmOrchestra/src/rhythmOrchestra_KJB/DynamicBeat.java
+++ rhythmOrchestra/src/rhythmOrchestra_KJB/DynamicBeat.java
@@ -1,15 +1,46 @@
 package rhythmOrchestra_KJB;
 
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class DynamicBeat extends JFrame{
+	
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
+	
 	public DynamicBeat() {
 		setTitle("Dynamic Beat");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);  
+		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/introbackground(title).jpg")).getImage();
+		
+		
+	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(introBackground,0,0,null) ;
+		
+		
+		
+		
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
+		
 		
 	}
 }
